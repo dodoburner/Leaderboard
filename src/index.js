25afbbbb9d40a9e758b066f33a107fec28eeca3a
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-07-16 15:00:00 +0200

    add plain html and css markup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './style.css'
+
+let scores = document.querySelectorAll('li')
+
+scores.forEach((score, index) => {
+  if (index % 2 === 0) {
+    score.classList.add('aqua')
+  } else {
+    score.classList.remove('aqua')
+  }
+})
